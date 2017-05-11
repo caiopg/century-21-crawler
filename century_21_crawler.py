a71b34bf3092a9b4e69ad0199d54c9c0aba068b4
Caio Gandra <caio.gandra@mandae.com.br>
2017-05-11 16:12:34 -0300

    Added multithreading.

--- century_21_crawler.py
+++ century_21_crawler.py
@@ -1,28 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
+from multiprocessing.dummy import Pool as ThreadPool
+from datetime import datetime
 import re
 import ast
 import pandas
 
+global base_url
 
-path = input("Please enter search path. Example: /real-estate/washington-nj/LCNJWASHINGTON\n")
-
-base_url = "http://www.century21.com"
-
-main_request = requests.get(base_url + path)
-main_content = main_request.content
-
-main = BeautifulSoup(main_content, "html.parser")
-total_results = main.find("div", {"class":"results-label"}).find("strong").text.strip()
-number_results = int(re.sub("[^0-9]", "", total_results))
-
-prop_list = []
-item_number = 0
-for res in range(0,  number_results, 20):
-    page_site = base_url +  path + "/?s=" + str(res) + "&o=listingdate-desc"
-    print(page_site)
-
-    main_request = requests.get(page_site)
+def get_properties_by_page(page):
+    main_request = requests.get(page)
+    print("FETCHED: " + page)
     main_content = main_request.content
 
     main = BeautifulSoup(main_content, "html.parser")
@@ -30,6 +18,7 @@ for res in range(0,  number_results, 20):
     regex = re.compile('infinite-item property-card*')
     all_properties = main.find_all("div", {"class":regex})
 
+    prop_list = []
     for item in all_properties:
         dict = {}
         address = item.find("div", {"class":"property-address"})
@@ -76,6 +65,7 @@ for res in range(0,  number_results, 20):
 
         features_link = price["href"]
         features_request = requests.get(base_url + features_link)
+        print("FETCHED: " + features_link)
         features_content = features_request.content
 
         features = BeautifulSoup(features_content, "html.parser")
@@ -86,11 +76,41 @@ for res in range(0,  number_results, 20):
             if(feature_name == "Lot Size"):
                 dict["Lot Size"] =feature_item.text.strip().split(": ")[1]
 
+        print("Finished: " + features_link)
         prop_list.append(dict)
 
-        item_number += 1
-        print("Item " + str(item_number) + " finished.")
+    print("Finished: " + page)
+    return prop_list
 
-df = pandas.DataFrame(prop_list)
+
+path = input("Please enter search path. Example: /real-estate/washington-nj/LCNJWASHINGTON\n")
+
+t0 = datetime.now()
+base_url = "http://www.century21.com"
+
+main_request = requests.get(base_url + path)
+main_content = main_request.content
+
+main = BeautifulSoup(main_content, "html.parser")
+total_results = main.find("div", {"class":"results-label"}).find("strong").text.strip()
+number_results = int(re.sub("[^0-9]", "", total_results))
+
+pages = []
+for res in range(0,  number_results, 20):
+    pages.append(base_url +  path + "/?s=" + str(res) + "&o=listingdate-desc")
+
+pool = ThreadPool(16)
+properties_list = pool.map(get_properties_by_page, pages)
+pool.close()
+pool.join()
+
+flattened = [val for sublist in properties_list for val in sublist]
+print("Number of properties: " + str(len(flattened)))
+
+df = pandas.DataFrame(flattened)
 df.to_excel("Output.xlsx")
-print("End")
+
+t1 = datetime.now()
+
+delta = t1 - t0
+print("Finished crawling! Total time: " + str(delta.total_seconds()) + " seconds")
