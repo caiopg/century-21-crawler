755ec8bdb17d6d9284313f841bf414bf5023a044
Caio Gandra <caio@digicash.tv>
2020-06-04 19:29:33 -0300

    Updated century-21 crawler

--- century_21_crawler.py
+++ century_21_crawler.py
@@ -8,6 +8,7 @@ import pandas
 
 global base_url
 
+
 def get_properties_by_page(page):
     main_request = requests.get(page)
     print("FETCHED: " + page)
@@ -16,49 +17,49 @@ def get_properties_by_page(page):
     main = BeautifulSoup(main_content, "html.parser")
 
     regex = re.compile('infinite-item property-card*')
-    all_properties = main.find_all("div", {"class":regex})
+    all_properties = main.find_all("div", {"class": regex})
 
     prop_list = []
     for item in all_properties:
         dict = {}
-        address = item.find("div", {"class":"property-address"})
-        if address != None:
+        address = item.find("div", {"class": "property-address"})
+        if address is not None:
             dict["Address"] = address.text.strip()
         else:
             dict["Address"] = None
 
-        locality = item.find("div", {"class":"property-city"})
-        if locality != None:
+        locality = item.find("div", {"class": "property-city"})
+        if locality is not None:
             dict["Locality"] = locality.text.strip()
         else:
             dict["Locality"] = None
 
-        price = item.find("a", {"class":"listing-price"})
-        if price != None:
-            dict["Price"] =  price.text.strip()
+        price = item.find("a", {"class": "listing-price"})
+        if price is not None:
+            dict["Price"] = price.text.strip()
         else:
             dict["Price"] = None
 
-        beds = item.find("div", {"class":"property-beds"})
-        if beds != None:
+        beds = item.find("div", {"class": "property-beds"})
+        if beds is not None:
             dict["Beds"] = beds.find("strong").text.strip()
         else:
             dict["Beds"] = None
 
-        square_feet = item.find("div", {"class":"property-sqft"})
-        if square_feet != None:
+        square_feet = item.find("div", {"class": "property-sqft"})
+        if square_feet is not None:
             dict["Area"] = square_feet.find("strong").text.strip()
         else:
             dict["Area"] = None
 
-        baths = item.find("div", {"class":"property-baths"})
-        if baths != None:
+        baths = item.find("div", {"class": "property-baths"})
+        if baths is not None:
             dict["Baths"] = baths.find("strong").text.strip()
         else:
             dict["Baths"] = None
 
-        half_baths = item.find("div", {"class":"property-half-baths"})
-        if half_baths != None:
+        half_baths = item.find("div", {"class": "property-half-baths"})
+        if half_baths is not None:
             dict["Half Baths"] = half_baths.find("strong").text.strip()
         else:
             dict["Half Baths"] = None
@@ -69,12 +70,12 @@ def get_properties_by_page(page):
         features_content = features_request.content
 
         features = BeautifulSoup(features_content, "html.parser")
-        feature_group = features.find("ul", {"class":"property-features"})
+        feature_group = features.find("ul", {"class": "property-features"})
 
         for feature_item in feature_group.find_all("li"):
             feature_name = feature_item.find("b").text.strip()
-            if(feature_name == "Lot Size"):
-                dict["Lot Size"] =feature_item.text.strip().split(": ")[1]
+            if (feature_name == "Lot Size"):
+                dict["Lot Size"] = feature_item.text.strip().split(": ")[1]
 
         print("Finished: " + features_link)
         prop_list.append(dict)
@@ -92,12 +93,12 @@ main_request = requests.get(base_url + path)
 main_content = main_request.content
 
 main = BeautifulSoup(main_content, "html.parser")
-total_results = main.find("div", {"class":"results-label"}).find("strong").text.strip()
+total_results = main.find("div", {"class": "results-label"}).find("strong").text.strip()
 number_results = int(re.sub("[^0-9]", "", total_results))
 
 pages = []
-for res in range(0,  number_results, 20):
-    pages.append(base_url +  path + "/?s=" + str(res) + "&o=listingdate-desc")
+for res in range(0, number_results, 20):
+    pages.append(base_url + path + "/?s=" + str(res) + "&o=listingdate-desc")
 
 pool = ThreadPool(16)
 properties_list = pool.map(get_properties_by_page, pages)
